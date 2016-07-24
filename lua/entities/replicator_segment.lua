6982887a5a16d54a8d3f1c3b475b9c289bebd9c9
CrishNate <demidemid1@gmail.com>
2016-07-24 21:04:56 +0300

    Wallclimbing and assembling of Replicators

--- lua/entities/replicator_segment.lua
+++ lua/entities/replicator_segment.lua
@@ -12,171 +12,146 @@ ENT.Category		= "Stargate"
 //if( CLIENT ) then killicon.Add( "ent_undertale_bone_throw", "undertale/killicon_bone", color_white ) end
 
 //
-//
+// segments to assemble replicator
 //
 local segments_count = 30
 
 
 function ENT:Initialize()
 	if( SERVER ) then
+		self:SetHealth( 10 )
+
 		self:SetModel( "models/stargate/replicators/replicator_segment.mdl" )
 		
 		self:PhysicsInit( SOLID_VPHYSICS )
 		self:SetMoveType( MOVETYPE_VPHYSICS )
 		self:SetSolid( SOLID_VPHYSICS )
-		
-		local phys = self:GetPhysicsObject()
-		phys:EnableGravity( true )
-		phys:Wake()
-		
-		self:Activate()
+		self:SetModelScale( self:GetModelScale() * 1.5, 0 )
+
 		self:SetUnFreezable( true ) 
 		
 		self:SetVar( "assembling", false )
-		self:SetVar( "used", false )
+		self:SetVar( "used", Entity( 0 ) )
+		
+		local phys = self:GetPhysicsObject()
+		if ( IsValid( phys ) ) then 
+			phys:EnableGravity( true )
+			phys:Wake()
+		end
 	end
 	
-	//if( CLIENT ) then
-	//end
-	/*
 	if( CLIENT ) then
-		local vec = self:GetPos()
-		local emitter = ParticleEmitter( vec, false )
-		
-		//effects/fire_cloud1
-		for cycles = 1, 10 do
-			local particle = emitter:Add( Material( "effects/fire_cloud1" ), vec )
-			if( particle ) then
-				particle:SetVelocity( VectorRand() * 40 )
-				particle:SetColor( 0, 100, 255 ) 
-				particle:SetLifeTime( 0 )
-				particle:SetDieTime( 1 )
-				particle:SetAngles( Angle( math.Rand( 0, 360 ), 0, 0 ) )
-				particle:SetAngleVelocity( Angle( math.Rand( -1, 1 ), 0, 0 ) )
-				particle:SetStartSize( 20 )
-				particle:SetEndSize( 10 )
-				particle:SetStartAlpha( 255 )
-				particle:SetEndAlpha( 0 )
-				particle:SetGravity( Vector( 0, 0, 60 ) )
-			end
-		end
-		
-		emitter:Finish()
 	end
-	*/
 end
 
 if( SERVER ) then
 	function ENT:Think()
-		
 		//
 		// Assembling segments it to a replicator
 		//
 		
-		if( !self:GetVar( "used" ) ) then
+		//self:SetColor( Color( 0, 0, 0 ) )
+
+		if( !self:GetVar( "used" ):IsValid() ) then
+			//self:SetColor( Color( 255, 255, 0) )
+
 			if( !self:GetVar( "assembling" ) ) then
 				self:NextThink( CurTime() + 1 )
+				//self:SetColor( Color( 0, 255, 0) )
 
-				local result = ents.FindInSphere( self:GetPos(), 50 )
+				local result = ents.FindInSphere( self:GetPos(), 200 )
 				local segments = {}
+				
+				table.Add( segments, { self } )
 				
 				for k, v in ipairs( result ) do
 					if( v:GetClass() == "replicator_segment" and 
 							v:GetVelocity():Length() < 10 and 
 								table.Count(segments) < segments_count and 
-									!v:GetVar( "assembling" ) && !v:GetVar( "used" ) ) then table.Add(segments, { v } ) end
+									!v:GetVar( "assembling" ) and !v:GetVar( "used" ):IsValid() and v != self ) then table.Add( segments, { v } ) end
 				end
 				
-				PrintMessage( HUD_PRINTTALK, table.Count(segments) )
-
-				if( table.Count(segments) == segments_count ) then
+				//PrintMessage( HUD_PRINTTALK, table.Count( segments ) )
+				//PrintTable( segments )
+				
+				if( table.Count( segments ) == segments_count ) then
 					self:SetVar( "assembling", true )
 					self:SetVar( "assembling_segments", segments )
 					
-					//local max = 0
 					local middle = Vector( 0, 0, 0 )
 					for k, v in ipairs( segments ) do
-						local a = v:GetPos()
-						middle:Add( v:GetPos() )
+						middle = middle + v:GetPos()
+						//v:SetColor( Color( 255, 0, 0 ) )
 						
-						//if( max < v:GetPos():Distance(self:GetPos()) ) then max = v:GetPos():Distance(self:GetPos()) end
-					
-						local phys = v:GetPhysicsObject()
-						v:Fire( "Kill", "", 4 )
-						phys:SetMaterial( "gmod_ice" )
-						if( v != self ) then v:SetVar( "used" ) end
+						if( v != self ) then v:SetVar( "used", self ) end
 					end
-					middle = middle / segments_count
 					
-					v:SetVar( "segments_center", middle )
-					//v:SetVar( "dMax", max )
+					middle = ( middle / segments_count )
+					
+					self:SetVar( "segments_middle", middle )
+					self:SetVar( "radius", 0 )
 				end
 
 			else
-				//local middle = self:GetVar( "segments_center" )
+				//self:SetColor( Color( 255, 0, 255 ) )
 				
 				self:NextThink( CurTime() )
 
 				local segments = self:GetVar( "assembling_segments" )
+				local middle = self:GetVar( "segments_middle" )
+				
+				self:SetVar( "radius", self:GetVar( "radius" ) + 2 )
+				
+				local inx = 0
 
-				if( !self:GetVar( "spawn" ) ) then
+				for k, v in ipairs( segments ) do
+
+					local vVel = v:GetVelocity()
+					if( v:GetPos():Distance( middle ) < self:GetVar( "radius" ) and vVel.z < 10 ) then
+						local dir = ( middle - v:GetPos() )
+						dir = Vector( dir.x, dir.y, 0 )
+						
+						dir:Normalize()
+						dir = dir * 50
+						
+						local phys = v:GetPhysicsObject()
+						phys:SetVelocity( Vector( dir.x, dir.y, vVel.z ))
+						phys:SetMaterial( "gmod_ice" )
+						
+						if( v:GetPos():Distance( middle ) < math.Rand( 0, 7 ) ) then
+							phys:EnableMotion( false )
+							phys:EnableCollisions( false )
+						end
+						
+						if( v:GetPos():Distance( middle ) <= 10 ) then inx = inx + 1 end
+						
+					end
+				end
+				
+				//------------- Spawning replicator
+				
+				if( inx == segments_count ) then
 					local ent = ents.Create( "replicator_worker" )
 					
 					if ( !IsValid( ent ) ) then return end
-					ent:SetPos( self:GetPos() + Vector( 0, 0, 5 ) )
+					ent:SetPos( middle + Vector( 0, 0, 6 ) )
+					ent:SetAngles( Angle( 0, math.Rand( 0, 360 ), 0 ) )
+					ent:SetOwner( self:GetOwner() )
+					ent:SetVar( "assemble", true )
 					ent:Spawn()
-					
-					self:SetVar( "radius", 0 )
-					self:SetVar( "spawn", true )
-				end
 				
-				self:SetVar( "radius", self:GetVar( "radius" ) + 1 )
-				
-				//local inx = 0
-				for k, v in ipairs( segments ) do
-				
-					if( v:GetPos():Distance( self:GetPos() ) < self:GetVar( "radius" ) ) then
-						local dir = ( self:GetPos() - v:GetPos() )
-						dir:Normalize()
-						dir = dir * 50 - v:GetVelocity() / 10
-						
-						local phys = v:GetPhysicsObject()
-						phys:SetVelocity( Vector( dir.x, dir.y, 0 ))
-						
-						if( v:GetPos():Distance( self:GetPos() ) < 5 ) then
-							phys:EnableMotion( false )
-							phys:EnableCollisions( false )
-							//v:Remove()
-							
-							if( v != self ) then v:Remove() end
-							table.remove( segments, k )
-						end
-					end
-				end
-				// Spawning replicator
-				/*
-				PrintMessage( HUD_PRINTTALK, inx )
-				if( inx > segments_count ) then
-					local button = ents.Create( "replicator_worker" )
-					
-					if ( !IsValid( button ) ) then return end
-					button:SetPos( self:GetPos() + Vector( 0, 0, 5 ) )
-					button:Spawn()
-					
-					self:SetVar( "used", true )
-					
 					for k, v in ipairs( segments ) do
-						v:Fire( "Kill", "", 2 )
 						local phys = v:GetPhysicsObject()
 						phys:EnableMotion( false )
+						phys:EnableCollisions( false )
+						v:Fire( "Kill", "", math.Rand( 0.5, 1.5 ) )
+						v:SetVar( "used", v )
 					end
+					
+					self:SetVar( "used", self )
+					//segments = {}
 				end
-				*/
-				
-				local dir = -self:GetVelocity() * 10
-				local phys = self:GetPhysicsObject()
-				
-				phys:SetVelocity( Vector( dir.x, dir.y, 0 ))
+				//*/
 			end
 		end
 
--- lua/entities/replicator_worker.lua
+++ lua/entities/replicator_worker.lua
@@ -12,76 +12,174 @@ ENT.AutomaticFrameAdvance = true
 
 //if( CLIENT ) then killicon.Add( "ent_undertale_bone_throw", "undertale/killicon_bone", color_white ) end
 
+local segments_count = 30
+
+function PlaySequence( self, seq )
+	local sequence = self:LookupSequence( seq )
+	
+	self:SetPlaybackRate( 1.0 )
+	self:SetSequence( sequence )
+	self:ResetSequence( sequence )
+	
+	return self:SequenceDuration( sequence )
+end
+
+function traceLineHull( startpos, endpos, _maxs, _mins, ignore )
+	local tr = util.TraceLine( {
+		start = startpos,
+		endpos = endpos,
+		mins = -_mins,
+		maxs = _maxs,
+		filter = function( ent ) if ( ent != ignore ) then return true end end
+	} )
+
+	return tr
+end
+
+function traceLine( startpos, endpos, ignore )
+	local tr = util.TraceLine( {
+		start = startpos,
+		endpos = endpos,
+		filter = function( ent ) if ( ent != ignore ) then return true end end
+	} )
+
+	return tr
+end
+
 function ENT:Initialize()
 	if( SERVER ) then
 		self:SetModel( "models/stargate/replicators/replicator_worker.mdl" )
-		//self:SetTrigger( true )
 		self:PhysicsInit( SOLID_VPHYSICS )
 		self:SetMoveType( MOVETYPE_VPHYSICS )
 		self:SetSolid( SOLID_VPHYSICS )
-		//self:SetModelScale( self:GetModelScale(), 0 )
-		//self:SetVar( "hit", false )
 		
 		local phys = self:GetPhysicsObject()
-		phys:EnableGravity( true )
 
-		local sequence = self:LookupSequence( "assembling" )
-		//self:ResetSequence( sequence )
-		self:SetPlaybackRate( 1.0 )
-		phys:EnableMotion( false ) 
-		phys:EnableCollisions( false )
-		//self:SetCycle( -1 )
+		self:SetHealth( 25 )
 		
-		self:SetSequence( sequence )
-		timer.Create( "standanim", self:SequenceDuration( sequence ), 0, function()
-			if( self:IsValid() ) then
-				local sequence = self:LookupSequence( "stand" )
-				phys:EnableMotion( true )
-				phys:EnableCollisions( true )
-				self:SetPlaybackRate( 1.0 )
-				self:SetSequence( sequence )
-			end
-		end )
+		self:SetVar( "rMove", false )
+		self:SetVar( "rMoveTo", self:GetPos() )
+			
+		phys:SetMaterial( "gmod_ice" )
+
+		if ( IsValid( phys ) and self:GetVar( "assemble" ) ) then 
+			phys:EnableGravity( true )
+			phys:EnableMotion( false ) 
+			//phys:EnableCollisions( false )
+		
+			timer.Simple(PlaySequence( self, "assembling" ), function()
+				if( self:IsValid() ) then
+					PlaySequence( self, "stand" )
+
+					if ( IsValid( phys ) ) then 
+						phys:EnableMotion( true )
+						phys:EnableCollisions( true )
+						phys:Wake()
+					end
+				end
+			end )
+		else
+			PlaySequence( self, "stand" )
+		end
 	end
 	
 	//if( CLIENT ) then
 		
 		
 	//end
-	
-	
-	/*
-	if( CLIENT ) then
-		local vec = self:GetPos()
-		local emitter = ParticleEmitter( vec, false )
-		
-		//effects/fire_cloud1
-		for cycles = 1, 10 do
-			local particle = emitter:Add( Material( "effects/fire_cloud1" ), vec )
-			if( particle ) then
-				particle:SetVelocity( VectorRand() * 40 )
-				particle:SetColor( 0, 100, 255 ) 
-				particle:SetLifeTime( 0 )
-				particle:SetDieTime( 1 )
-				particle:SetAngles( Angle( math.Rand( 0, 360 ), 0, 0 ) )
-				particle:SetAngleVelocity( Angle( math.Rand( -1, 1 ), 0, 0 ) )
-				particle:SetStartSize( 20 )
-				particle:SetEndSize( 10 )
-				particle:SetStartAlpha( 255 )
-				particle:SetEndAlpha( 0 )
-				particle:SetGravity( Vector( 0, 0, 60 ) )
-			end
-		end
-		
-		emitter:Finish()
-	end
-	*/
 end
 
 if( SERVER ) then
-	function ENT:Think()
-		//self:SendViewModelMatchingSequence( sequence )
 
+	function ENT:OnTakeDamage( dmginfo )
+		//self:TakeDamage( dmginfo:GetDamage(), "", "" )
+		local damage = dmginfo:GetDamage()
+		
+		self:SetHealth( self:Health() - damage )
+
+		if( self:Health() <= 0 ) then
+			local phys = self:GetPhysicsObject()
+			phys:EnableCollisions( false )
+
+			for i = 1, segments_count do
+				local ent = ents.Create( "replicator_segment" )
+				
+				if ( !IsValid( ent ) ) then return end
+				ent:SetPos( self:GetPos() + VectorRand() * 3 )
+				ent:SetOwner( self:GetOwner() )
+				ent:Spawn()
+				
+				local phys = ent:GetPhysicsObject()
+				phys:Wake()
+				phys:SetVelocity( VectorRand() * damage * 4 )
+				//local phys = ent:GetPhysicsObject()
+			end
+			
+			self:Remove()
+		end
+	end
+	
+	function ENT:Think()
+		//
+		// Moving a replicator
+		//
+		
+		if( SERVER ) then
+			local phys = self:GetPhysicsObject()
+			
+			
+			//
+			// Wall climbing system
+			//											
+			local ground = traceLineHull( self:GetPos(), 
+											self:GetPos() - self:GetUp() * 10, 
+											Vector( 15, 15, 15 ), -Vector( 15, 15, 15 ), self )
+			phys:EnableGravity( !ground.Hit )
+			
+			local velocity = Vector()
+			if( ground.Hit ) then
+				//print( ground.HitPos:Distance( self:GetPos() ))
+				velocity:Add( -self:GetUp() * ground.HitPos:Distance( self:GetPos() ) * 2 )
+
+				local forward = traceLineHull( self:GetPos(), 
+												self:GetPos() + self:GetForward() * 20, 
+												Vector( 3, 3, 3 ), -Vector( 3, 3, 3 ), self )
+												
+				//if( forward.Hit ) then phys:SetAngles( self:LocalToWorldAngles( Angle( -20, 0, 0 ) )  ) else	
+				if( forward.Hit ) then phys:AddAngleVelocity( Vector( 0, -1000, 0 ) - phys:GetAngleVelocity() ) else	
+				
+					local fordown = traceLineHull( self:GetPos() + self:GetForward() * 5, 
+													self:GetPos() + self:GetForward() * 5 - self:GetUp() * 15, 
+													Vector( 3, 3, 3 ),  -Vector( 3, 3, 3 ), self )
+
+					//if( !fordown.Hit ) then phys:SetAngles( self:LocalToWorldAngles( Angle( 20, 0, 0 ) ) ) end
+					if( !fordown.Hit ) then phys:AddAngleVelocity( Vector( 0, 1000, 0 ) - phys:GetAngleVelocity() ) end
+				end
+			else
+				local ceiling = traceLineHull( self:GetPos(), 
+												self:GetPos() + self:GetUp() * 20, 
+												Vector( 10, 10, 10 ), -Vector( 10, 10, 10 ), self )
+					
+				if( ceiling.Hit ) then phys:AddAngleVelocity( Vector( math.Rand( 0, 1100 ), math.Rand( 0, 500 ), 0 ) - phys:GetAngleVelocity() ) end
+			end
+			
+			if( Entity( 1 ):KeyPressed( IN_RELOAD ) ) then
+				PlaySequence( self, "run" )
+				self:SetVar( "rMove", true )
+			end
+			
+			if( self:GetVar( "rMove" ) ) then
+				velocity:Add( self:GetForward() * 5 )
+			end
+
+			velocity:Add( -self:GetVelocity() / 10 )
+			
+			if( ground.Hit ) then
+				phys:AddVelocity( velocity )
+				phys:AddAngleVelocity( -phys:GetAngleVelocity() / 5 )			
+			end
+		end
+		
 		self:NextThink( CurTime() )
 		return true
 	end
